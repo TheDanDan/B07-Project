42129e49cf3c5d9770a53d29f3dae217bc9aa724
panrach <panrrachel@gmail.com>
2023-07-25 00:21:49 -0400

    Shoppers and owners can create accounts. Accounts get added to firebase.
    Checks to see if password & username field is filled in.
    
    Does not check to see if username exists in the firebase.

--- B07Project/app/src/main/java/com/example/b07project/ActivityAccountType.java
+++ B07Project/app/src/main/java/com/example/b07project/ActivityAccountType.java
@@ -1,42 +1,127 @@
 package com.example.b07project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
 
 public class ActivityAccountType extends AppCompatActivity {
-    TextView textView2;
+    TextView textView;
 
     EditText usernameInput;
     EditText passwordInput;
+    Button btnInsertData;
+    DatabaseReference ref;
+    FirebaseDatabase firebaseDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_type);
-
-        usernameInput = (EditText) findViewById(R.id.editTextUsernameSignup);
-        passwordInput = (EditText) findViewById(R.id.editTextPasswordSignup);
     }
 
     public void onClickCreateShopper(View view) {
-        // to do
-        // open ref Shopper
-        // put products under user in the firebase
-        // Retrieve the username and password input
+        usernameInput = (EditText) findViewById(R.id.editTextUsernameSignup);
+        passwordInput = (EditText) findViewById(R.id.editTextPasswordSignup);
 
-        textView2.setText(usernameInput.getText());
-        System.out.println("Button clicked");
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        ref = firebaseDatabase.getReference("Shoppers");
+
+        btnInsertData = findViewById(R.id.buttonCreateShopper);
+
+        // getting username and password to a string
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        btnInsertData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertShopper();
+            }
+        });
 
     }
 
+    private void insertShopper() {
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        if (username.length() == 0 || password.length() == 0) {
+            Toast.makeText(ActivityAccountType.this, "You must enter a username or password!", Toast.LENGTH_SHORT).show();
+        }
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (username.length() == 0 || password.length() == 0) {
+                    throw new RuntimeException();
+                }
+                ref.child(username).child("password").setValue(password);
+                Toast.makeText(ActivityAccountType.this, "data added", Toast.LENGTH_SHORT).show();
+                usernameInput.getText().clear();
+                passwordInput.getText().clear();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ActivityAccountType.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     public void onClickCreateOwner(View view) {
-        // to do
-        // open ref to Owner
+        usernameInput = (EditText) findViewById(R.id.editTextUsernameSignup);
+        passwordInput = (EditText) findViewById(R.id.editTextPasswordSignup);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        ref = firebaseDatabase.getReference("Owners");
+
+        btnInsertData = findViewById(R.id.buttonCreateOwner);
+
+        // getting username and password to a string
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        btnInsertData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertOwner();
+            }
+        });
     }
 
-    // adding ppl method
+    private void insertOwner() {
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        if (username.length() == 0 || password.length() == 0) {
+            Toast.makeText(ActivityAccountType.this, "You must enter a username or password!", Toast.LENGTH_SHORT).show();
+        }
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ref.child(username).child("password").setValue(password);
+                Toast.makeText(ActivityAccountType.this, "data added", Toast.LENGTH_SHORT).show();
+                usernameInput.getText().clear();
+                passwordInput.getText().clear();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ActivityAccountType.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
