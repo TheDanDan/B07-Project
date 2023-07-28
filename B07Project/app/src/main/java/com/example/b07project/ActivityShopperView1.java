e77f41be3727725dc4ffc943728a6f11892406b3
alysaleong <leong.alysa@gmail.com>
2023-07-27 23:56:13 -0400

    Create the basic interface for a shopper - a navigation bar, the home page, a search page, and their cart

--- B07Project/app/src/main/java/com/example/b07project/ActivityShopperView1.java
+++ B07Project/app/src/main/java/com/example/b07project/ActivityShopperView1.java
@@ -1,14 +1,47 @@
 package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.b07project.databinding.ActivityShopperView1Binding;
+
 public class ActivityShopperView1 extends AppCompatActivity {
+
+    ActivityShopperView1Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopper_view1);
+        binding = ActivityShopperView1Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new FragmentShopperHome());
+        binding.bottomNavigationView2.setBackground(null);
+
+        binding.bottomNavigationView2.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home)
+                replaceFragment(new FragmentShopperHome());
+            else if (item.getItemId() == R.id.search)
+                replaceFragment(new FragmentShopperSearch());
+            else
+                replaceFragment(new FragmentShopperCart());
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+
 }
