c3fef80f88922b4d3071e0eae3f19f988e247a0c
TheDanDan <77023480+TheDanDan@users.noreply.github.com>
2023-07-28 15:21:45 -0400

    Worked on owner pages
    Navigation Bar for owner and home page for owner

--- B07Project/app/src/main/java/com/example/b07project/ActivityOwnerView1.java
+++ B07Project/app/src/main/java/com/example/b07project/ActivityOwnerView1.java
@@ -1,15 +1,20 @@
 package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.b07project.databinding.ActivityShopperView1Binding;
+
 public class ActivityOwnerView1 extends AppCompatActivity {
 
     static String username; //will be passed from previous activity
-
+    ActivityShopperView1Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,31 @@ public class ActivityOwnerView1 extends AppCompatActivity {
 
         TextView msgWelcome = (TextView) findViewById(R.id.textViewWelcomeTestOwner); //THIS LINE IS PURELY FOR TESTING
         msgWelcome.setText("Welcome " + username + "!");                              //FEEL FREE TO REMOVE LATER ! :-)
+
+        binding = ActivityShopperView1Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new FragmentShopperHome());
+        binding.bottomNavigationView2.setBackground(null);
+
+        binding.bottomNavigationView2.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home)
+                replaceFragment(new FragmentShopperHome());
+            else if (item.getItemId() == R.id.search)
+                replaceFragment(new FragmentShopperSearch());
+            else
+                replaceFragment(new FragmentShopperCart());
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
