f04565b4f089b2e2feaeb2c5744ee7dd85c266ee
panrach <panrrachel@gmail.com>
2023-07-31 17:31:07 -0400

    users can add items to cart
    - if item exists update quantity
    - if item does not exist in cart, create an instance of item in the cart
    - Janani spent a lot of time helping me debug

--- B07Project/app/src/main/java/com/example/b07project/FragmentShopperCart.java
+++ B07Project/app/src/main/java/com/example/b07project/FragmentShopperCart.java
@@ -4,19 +4,44 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Button;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FragmentShopperCart extends Fragment {
 
     String username;
+    Button btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         username = this.getArguments().getString("username");
-        return inflater.inflate(R.layout.fragment_shopper_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_shopper_cart, container, false);
+
+        System.out.println("1");
+        btn = view.findViewById(R.id.button3);
+
+        String username = "alysa";
+        String id = "Na_Eijs3x7h4EMlhSr3p";
+        String ownerUsername = "a";
+
+        ShopperItem item = new ShopperItem();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               item.addToCart(id, 3, username, ownerUsername);
+                Log.d("abc", String.valueOf(item.addSuccessful));
+            }
+        });
+
+        return view;
     }
 }
--- B07Project/app/src/main/java/com/example/b07project/ShopperItem.java
+++ B07Project/app/src/main/java/com/example/b07project/ShopperItem.java
@@ -1,11 +1,57 @@
 package com.example.b07project;
 
+import static androidx.databinding.DataBindingUtil.setContentView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.*;
+
 public class ShopperItem {
-    private Item item;
+    private String id;
     private int quantity;
     private int status; // 0 for not yet ordered (i.e. in cart), 1 for ordered but not read, 2 for ready
+    Button btn;
+
+    static private DatabaseReference ref = FirebaseDatabase.getInstance("https://b07-project-3237a-default-rtdb.firebaseio.com/").getReference("Shoppers");
 
     // constructor, setters, getters, equals, hashcode
+    // getters
+
+    public ShopperItem() {
+    }
+
+    public ShopperItem(int quantity, String id) {
+        this.quantity = quantity;
+        this.id = id;
+        this.status = 0;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
     public boolean removeFromCart(int quantity, String username) {
         // only works if status is 0
         // reduce this.quantity by quantity
@@ -13,9 +59,33 @@ public class ShopperItem {
         return false;
     }
 
-    public boolean addToCart(String id, int quantity, String username) {
+    public void addToCart(String id, int quantity, String username, String ownerUsername) {
         // add item associated with id to database if not already there
         // if already there, increase quantity of item
-        return false;
+
+        ref = FirebaseDatabase.getInstance().getReference("Shoppers");
+        DatabaseReference cart_ref = ref.child(username).child("cart");
+
+        ref.child(username).child("cart").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // item exists
+                if (snapshot.exists()) {
+                    Integer currentQuantity = snapshot.child("quantity").getValue(Integer.class);
+                    int newQuantity = (int) currentQuantity + quantity;
+                    ref.child(username).child("cart").child(id).child("quantity").setValue(newQuantity);
+
+                } else {
+                    ShopperItem shopperItem = new ShopperItem(quantity, id);
+                    ref.child(username).child("cart").child(id).setValue(shopperItem);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(btn.getContext(), "Failed to add to cart " + error, Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
     }
 }
